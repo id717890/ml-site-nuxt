946cba4cc2f0289e440ae19b349e73f672ba479f
Rha <rnnchik@gmail.com>
2021-04-04 23:58:08 +0200

    добавлены кнопки в футер, поправлены тени в блоках

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,15 @@
             <div class="flex">
                 <div class="box box_1">
                     <a href="/"><img src="../assets/img/logo_white.png" alt="Logo"></a>
-                    <div class="copi">© {{ new Date().getFullYear() }} Все права защищены</div>
+                    <div class="copi">© 2010-{{ new Date().getFullYear() }} Все права защищены</div>
                 </div>
-                <div class="box box_2"><a href="/" title="Подробнее" class="pers">Обработка персональных данных</a></div>
+                <div class="box box_2">
+                    <div class="btn_block flex">
+                        <button type="button" class="btn bg_tr btn-consultation">Заказать звонок</button>
+                        <button type="button" class="btn btn-order">Узнать стоимость</button>
+                    </div>
+                    <a href="/" title="Подробнее" class="pers">Обработка персональных данных</a>
+                    </div>
                 <div class="box box_3">
                     <div class="title_footer">Контактная информация:</div>
                     <a href="tel:+74999901863" class="tel" title="Позвонить">+7 (499) 990-18-63</a>
